39b12fba7d32e2f79cf281872af378a2850ba1ce
Loaff! <69651069+Loaffoftoast@users.noreply.github.com>
2026-08-05 10:28:40 -0400

    Graph Math

--- Grid Zoom Math.py
+++ Grid Zoom Math.py
@@ -3,9 +3,9 @@
 import pygame
 import sys
 
-count = 1
-number = 1
-numberLast = 1
+count = 2
+currInterval = 2
+prevInterval = 1
 zoomLevel = 1
 
 screen = pygame.display.set_mode((800, 500), pygame.RESIZABLE)
@@ -22,26 +22,30 @@ while running:
         if event.key == pygame.K_UP and pressed == False:  # Scroll Up
             zoomLevel = zoomLevel * 1.1
             pressed = True
+            if (zoomLevel > currInterval):
+                prevInterval = currInterval
+                count = count + 1
+                if count % 3 == 0: currInterval = currInterval * 2.5
+                else: currInterval = currInterval * 2
+                
         elif event.key == pygame.K_DOWN and pressed == False:  # Scroll Down
             zoomLevel = zoomLevel * 0.9
             pressed = True
+            if (zoomLevel < prevInterval):
+                count = count - 1
+                if ((count + 1) % 3) == 0: currInterval = currInterval / 2.5
+                else: currInterval = currInterval / 2
+                
+                if count % 3 == 0: prevInterval = prevInterval / 2.5
+                else: prevInterval = prevInterval / 2
+                
+
         if event.key == pygame.K_ESCAPE:
             running = False
     if not event.type == pygame.KEYDOWN:
         pressed = False
     
-    if zoomLevel > number:
-        numberLast = number
-        count += 1
-        if count % 3 == 0:
-            number = number * 2.5
-        else:
-            number = number * 2
-    elif zoomLevel < numberLast:
-        count -= 1
-        if count % 3 == 0:
-            number = number / 2.5
-        else:
-            number = number / 2
+
+
         
-    print(zoomLevel,number)
+    print(count, zoomLevel, currInterval, prevInterval)
